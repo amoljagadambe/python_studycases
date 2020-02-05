729ec189bdef1d2875ad3e6f44beefbf1ba41cd4
amoljagadambe <amoljagadambe0076@gmail.com>
2020-02-05 20:23:24 +0530

    Namedtuple access methods types

--- advance-python/datastructures/namedtuple_import.py
+++ advance-python/datastructures/namedtuple_import.py
@@ -7,4 +7,15 @@ namedtuple(typename, field_names, *, verbose=False, rename=False, module=None)
 
 named_a = namedtuple('courses', ['name', 'technology'])
 values = named_a('Machine Learning', 'python')
-print(values)
+
+'''
+Namedtuple access methods
+'''
+# Access using name
+print(values.name)  # output : Machine Learning
+
+# Access using index
+print(values[1])  # output : python
+
+# Access using getattr()
+print(getattr(values, 'name'))  # output : Machine Learning
