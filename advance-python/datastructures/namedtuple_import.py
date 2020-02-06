5fb5d8aa03d35b63b50d95683f56146ddfd1f273
amoljagadambe <amoljagadambe0076@gmail.com>
2020-02-06 20:07:58 +0530

    Conversion Operations added for namedtuple

--- advance-python/datastructures/namedtuple_import.py
+++ advance-python/datastructures/namedtuple_import.py
@@ -19,3 +19,27 @@ print(values[1])  # output : python
 
 # Access using getattr()
 print(getattr(values, 'name'))  # output : Machine Learning
+
+
+# Conversion Operations
+
+it_list = ['Tensorflow', 'ML']
+sample_dict = {'name': "PowerBI", 'technology': 'Tableu'}
+
+'''
+using _make() to return namedtuple()
+_make() :- This function is used to return a namedtuple() from the iterable passed as argument.
+'''
+print(named_a._make(it_list))  # output : courses(name='Tensorflow', technology='ML')
+
+'''
+using _asdict() to return an OrderedDict()
+_asdict() :- This function returns the OrdereDict() as constructed from the mapped values of namedtuple().
+'''
+print(values._asdict())  # output :  OrderedDict([('name', 'Machine Learning'), ('technology', 'python')])
+
+'''
+using ** operator to return namedtuple from dictionary
+using “**” (double star) operator :- This function is used to convert a dictionary into the namedtuple().
+'''
+print(named_a(**sample_dict))  # output :  courses(name='PowerBI', technology='Tableu')
