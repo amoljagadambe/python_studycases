4ee1ce37f87d8e71b3a9a8d8cf5fd85f7f300e7c
amoljagadambe <amoljagadambe0076@gmail.com>
2020-02-10 15:32:34 +0530

    finding the k largest number in heap queue

--- advance-python/datastructures/heapq_tutorials.py
+++ advance-python/datastructures/heapq_tutorials.py
@@ -1,10 +1,11 @@
 import heapq
 
-
 li_data = [5, 7, 9, 1, 3]
+li = [5, 7, 9, 4, 3, 1]
 
 # using heapify to convert list into heap
 heapq.heapify(li_data)
+heapq.heapify(li)
 print(li_data)
 
 '''
@@ -20,3 +21,31 @@ function is used to remove and return the
 smallest element from heap. 
 '''
 print(heapq.heappop(li_data))
+
+'''
+using heappushpop() to push and pop items simultaneously
+pops 2
+'''
+print(heapq.heappushpop(li, 2))
+
+'''
+using heapreplace() to push and pop items simultaneously
+element is first popped, then element is pushed.i.e, the 
+value larger than the pushed value can be returned.
+'''
+print(heapq.heapreplace(li, 5))
+
+'''
+ function is used to return the k largest elements from 
+ the iterable specified and satisfying the key if mentioned.
+ returns the list
+'''
+largest = heapq.nlargest(3, li)
+print(largest)  # output: [9, 7, 5]
+
+'''
+unction is used to return the k smallest elements from 
+the iterable specified and satisfying the key if mentioned.
+'''
+smallest = heapq.nsmallest(2, li_data)
+print(smallest)  # output:[3, 4]
