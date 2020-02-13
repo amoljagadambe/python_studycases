3ce191b787356c95d86e3db9e79f3fdcde278ff3
amoljagadambe <amoljagadambe0076@gmail.com>
2020-02-13 18:12:56 +0530

    initialzation and substraction mathod added

--- advance-python/datastructures/collections/counter_tutorials.py
+++ advance-python/datastructures/collections/counter_tutorials.py
@@ -1,7 +1,25 @@
 from collections import Counter
 
-data = ['B','B','A','B','C','A','B','B','A','C']
+# With sequence of items
+data = ['B', 'B', 'A', 'B', 'C', 'A', 'B', 'B', 'A', 'C']
 out_data = Counter(data)
-print(out_data)
+print(out_data)  # output:Counter({'B': 5, 'A': 3, 'C': 2})
 
+# with keyword arguments
+print(Counter(A=3, B=5, C=2))  # output:Counter({'B': 5, 'A': 3, 'C': 2})
+
+# Creating the empty counter
+
+count = Counter()
+count.update(data)
+print(count)  # output:Counter({'B': 5, 'A': 3, 'C': 2})
+
+# adding the value into same count
+
+count.update(['A', 'C', 'Z'])
+print(count)  # output:Counter({'B': 5, 'A': 4, 'C': 3, 'Z': 1})
+
+# Counter subtraction
+count.subtract(out_data)
+print(count)  # output:Counter({'A': 1, 'C': 1, 'Z': 1, 'B': 0})
 
