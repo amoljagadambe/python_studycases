e6df3d9e956de6d27d046ad49be11d04fb5da299
amoljagadambe <amoljagadambe0076@gmail.com>
2020-02-17 15:12:14 +0530

    implicit Memoization using lru_cache for fibonacci series

--- problem-study-cases/Fibonacci_with_Memoization.py
+++ problem-study-cases/Fibonacci_with_Memoization.py
@@ -1,29 +1,53 @@
-'''
+"""
 We will define the Memoization concept explicitly and implicitly in this program
-'''
+"""
 # Method 1) explicitly using cache dict
 
-fibonacci_cache = {}
+# fibonacci_cache = {}
+#
+#
+# def fibonacci(n):
+#     # If we have the value of n'th term then return the value
+#     if n in fibonacci_cache:
+#         return fibonacci_cache[n]
+#
+#     # compute the N'th value
+#     if n == 1:
+#         value = 1
+#     elif n == 2:
+#         value = 1
+#     else:
+#         value = fibonacci(n - 1) + fibonacci(n - 2)
+#
+#     # store the value of the N'th term
+#     fibonacci_cache[n] = value
+#     return value
+#
+#
+# print("Fibonacci sequence:")
+# for i in range(1, 101):
+#     print(i, ':', fibonacci(i))
+
+# Method 2) implicitly using cache lru_cache
+from functools import lru_cache
 
 
+@lru_cache(maxsize=1000)
 def fibonacci(n):
-    # If we have the value of n'th term then return the value
-    if n in fibonacci_cache:
-        return fibonacci_cache[n]
+    # Check for the input is a positive integer
+    if type(n) != int:
+        raise TypeError('n must be of type integer')
+    if n < 1:
+        raise ValueError('n must be a positive integer')
 
-    # compute the N'th value
+    # compute the n'th term
     if n == 1:
-        value = 1
+        return 1
     elif n == 2:
-        value = 1
+        return 1
     else:
-        value = fibonacci(n - 1) + fibonacci(n - 2)
-
-    # store the value of the N'th term
-    fibonacci_cache[n] = value
-    return value
+        return fibonacci(n - 1) + fibonacci(n - 2)
 
 
-print("Fibonacci sequence:")
-for i in range(1, 101):
-    print(i, ':', fibonacci(i))
+for i in range(1, 51):
+    print(fibonacci(i))
